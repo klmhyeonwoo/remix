aa44d9bb8cf58a0662bbdb07fa513c7854595764
klmhyeonwoo <gentlemonster77@likelion.org>
2024-10-22 00:17:46 +0900

    feat(analytics): #4 Fix Google Analytics Feature

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,11 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLocation } from "@remix-run/react";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import ReactGA from "react-ga";
 
 const queryClient = new QueryClient();
+const googleTrackingId = import.meta.env.VITE_GOOGLE_ANALYTICS as string;
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,15 +13,29 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/feeed.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
           as="style"
           crossOrigin="true"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
         />
+        {googleTrackingId && (
+          <Fragment>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleTrackingId}`}></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${googleTrackingId}');
+                `,
+              }}
+            />
+          </Fragment>
+        )}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="viewport" content="width=device-width" />
         <meta name="theme-color" content="#000000" />
         <meta property="og:type" content="website" />
@@ -46,6 +62,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize(googleTrackingId);
+    ReactGA.pageview(location.pathname);
+  }, [location]);
   return (
     <Fragment>
       <QueryClientProvider client={queryClient}>
